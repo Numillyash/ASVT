85bbe9ec3b315d34adbad4ca78ef4c9553362c29
Numillyash <ulanovsky.zhora@ya.ru>
2023-04-15 13:54:55 +0300

    опять ничего не работает

--- ASVT_LAB_1/Arduino/Arduino.ino
+++ ASVT_LAB_1/Arduino/Arduino.ino
@@ -16,25 +16,146 @@
 int led = 5;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
-
+int razr_cur = 0;
+int num_cur = 0;
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(A5, INPUT);
+  pinMode(8, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void writeByte(int razr, int num, bool printReg)
+{
+  switch(razr)
+  {
+    case 0:
+      PORTB = 0b11111100;
+      break; 
+    case 1:
+      PORTB = 0b11111001;
+      break; 
+    case 2:
+      PORTB = 0b11110101;
+      break; 
+    case 3:
+      PORTB = 0b11101101;
+      break; 
+    default:
+      break;
+  }  
+  if(printReg)
+  {PORTD = num;
+      PORTC = num % 4;}
+  else
+  switch(num)
+  {
+    case 0:
+      PORTD = 0x7E;
+      PORTC = 0x7E % 4;
+      break;
+    case 1:
+      PORTD = 0x0C;
+      PORTC = 0x0C % 4;
+      break;
+    case 2:
+      PORTD = 0xB6;
+      PORTC = 0xB6 % 4;
+      break;
+    case 3:
+      PORTD = 0x9E;
+      PORTC = 0x9E % 4;
+      break;
+    case 4:
+      PORTD = 0xCC;
+      PORTC = 0xCC % 4;
+      break;
+    case 5:
+      PORTD = 0xDA;
+      PORTC = 0xDA % 4;
+      break;
+    case 6:
+      PORTD = 0xFA;
+      PORTC = 0xFA % 4;
+      break;
+    case 7:
+      PORTD = 0x0E;
+      PORTC = 0x0E % 4;
+      break;
+    case 8:
+      PORTD = 0xFE;
+      PORTC = 0xFE % 4;
+      break;
+    case 9:
+      PORTD = 0xDE;
+      PORTC = 0xDE % 4;
+      break;
+    case 10:
+      PORTD = 0xEE;
+      PORTC = 0xEE % 4;
+      break;
+    case 11:
+      PORTD = 0xFF;
+      PORTC = 0xFF % 4;
+      break;
+    case 12:
+      PORTD = 0x72;
+      PORTC = 0x72 % 4;
+      break;
+    case 13:
+      PORTD = 0x7F;
+      PORTC = 0x7F % 4;
+      break;
+    case 14:
+      PORTD = 0xF2;
+      PORTC = 0xF2 % 4;
+      break;
+    case 15:
+      PORTD = 0xE2;
+      PORTC = 0xE2 % 4;
+      break;
+    default:
+      PORTD = num;
+      PORTC = num % 4;
+      break;
+  }
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // set the brightness of pin 9:
-  analogWrite(led, brightness);
+//  analogWrite(led, brightness);
 
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
-
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
-  }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
+//  // change the brightness for next time through the loop:
+//  brightness = brightness + fadeAmount;
+//
+//  // reverse the direction of the fading at the ends of the fade:
+//  if (brightness <= 0 || brightness >= 255) {
+//    fadeAmount = -fadeAmount;
+//  }
+//  // wait for 30 milliseconds to see the dimming effect
+//  for(num_cur = 0; num_cur < 16; num_cur++){
+//  writeByte(razr_cur, num_cur);
+//  delay(1000);}
+//  razr_cur = (razr_cur+1)%4;
+  int anRe = analogRead(A5)/4;
+  writeByte(0, anRe, true);
+  delay(1);
+  writeByte(1, anRe, true);
+  delay(1);
+  writeByte(2, (anRe/16), false);
+  delay(1);
+  writeByte(3, anRe%16, false);
+  delay(1);
 }
--- ASVT_LAB_PWM/ADC.asm
+++ ASVT_LAB_PWM/ADC.asm
@@ -8,9 +8,18 @@
 .def	wreg		=R16
 .def	duration	=R17
 .def	wreg2		=R18
-
+.def printByte = R26
+.def hbyteNum = R27
+.def lbyteNum = R28
+.def delay_reg = R29
+.def tmp_reg = R20
 init:
-
+	ldi wreg, 0xFF;(1<<DDB3)|(1<<DDB2)|(1<<DDB4)|(1<<DDB0)
+	OUT  DDRB, wreg
+	ldi wreg, 0b00000011
+	OUT  DDRC, wreg
+	ldi wreg, 0b11100010
+	OUT  PORTB, wreg
 	ldi		wreg,low(RAMEND)             
 	out		SPL,wreg
 
@@ -23,7 +32,7 @@ init:
 	ldi wreg, 0b10000011		; ���. ���, ������. ������., ���������� ���������, ��/8 (125 kHz)
 	STS ADCSRA, wreg
 
-	ldi wreg, 0b01101110		; AVcc, ����. ����. �� Aref, ������. �� ���. ����, channel 1.23V bg
+	ldi wreg, 0b01100101		; AVcc, ����. ����. �� Aref, ������. �� ���. ����, channel 1.23V bg
 	STS ADMUX, wreg
 
 	rcall adc_convert
@@ -31,7 +40,7 @@ init:
     
 	ldi		wreg,0x00		
 	out		PORTB,wreg			; ������ 0 � ����
-	ldi		wreg,0b00000010		; PortB.1 (OC1A) - Output
+	ldi		wreg,0xFF		; PortB.1 (OC1A) - Output
 	out		DDRB,wreg		
 	
 	ldi wreg, 0b10000001		; �����. 8 ������ ���
@@ -39,53 +48,270 @@ init:
 
 	ldi wreg, 0b00001001		; CK/1
 	STS TCCR1B, wreg
-
-
+	SEI
+	LDI R19, 250
 main:
-	
-	ldi wreg, 250				; 25mS
-	rcall delay
+	CLR R24
+	CLR printByte
+	OUT PORTD, printByte
+	OUT PORTC, R24
 
+	; GET ADC
 	ldi wreg, 0b01100101		; ADC Channel 5 (PortC.5 ; pin 28)
 	rcall adc_convert
+
 	
+	
+	; 1-s digit for PWM
+	ldi R30, 0b11111110
 	ldi wreg2, 0x00
+	OUT PORTD, wreg2
 	STS OCR1AH, wreg2
 	STS OCR1AL, wreg
+	OUT PORTB, R30
+	rcall delay_setup
+	; Clear PWM
+	ldi wreg2, 0x00
+	STS OCR1AH, wreg2
+	STS OCR1AL, wreg2
+	; SEND adc to 2-d digit
+	ldi R30, 0b11111001
+	CLR R24
+	BST wreg, 0
+	BLD R24, 0
+	BST wreg, 1
+	BLD R24, 1 
+	OUT PORTD, wreg
+	OUT PORTC, R24
+	OUT PORTB, R30
+	rcall delay_setup
+
+	; convert ADC to 2 bytes
+	call movBytes ; set bytes
+	
+	; hbyte
+	ldi R30, 0b11110101 ; 3-d digit
+	MOV printByte, hbyteNum
+	call printByte_func
+	CLR R24
+	BST printByte, 0
+	BLD R24, 0
+	BST printByte, 1
+	BLD R24, 1 
+	OUT PORTB, R30
+	OUT PORTD, printByte
+	OUT PORTC, R24
+	rcall delay_setup
+
+	; lbyte
+	ldi R30, 0b11101101 ; 4-d digit
+	MOV printByte, lbyteNum
+	call printByte_func
+	CLR R24
+	BST printByte, 0
+	BLD R24, 0
+	BST printByte, 1
+	BLD R24, 1 
+	OUT PORTB, R30
+	OUT PORTD, printByte
+	OUT PORTC, R24
+	rcall delay_setup
+
+
 
 rjmp main
 
+printByte_func:
+	ldi R22, 15
+	CP printByte, R22
+	BRSH print_F
+	ldi R22, 14
+	CP printByte, R22
+	BRSH print_E
+	ldi R22, 13
+	CP printByte, R22
+	BRSH print_D
+	ldi R22, 12
+	CP printByte, R22
+	BRSH print_C
+	ldi R22, 11
+	CP printByte, R22
+	BRSH print_B
+	ldi R22, 10
+	CP printByte, R22
+	BRSH print_A
+	ldi R22, 9
+	CP printByte, R22
+	BRSH print_9
+	ldi R22, 8
+	CP printByte, R22
+	BRSH print_8
+	ldi R22, 7
+	CP printByte, R22
+	BRSH print_7
+	ldi R22, 6
+	CP printByte, R22
+	BRSH print_6
+	ldi R22, 5
+	CP printByte, R22
+	BRSH print_5
+	ldi R22, 4
+	CP printByte, R22
+	BRSH print_4
+	ldi R22, 3
+	CP printByte, R22
+	BRSH print_3
+	ldi R22, 2
+	CP printByte, R22
+	BRSH print_2
+	ldi R22, 1
+	CP printByte, R22
+	BRSH print_1
+	ldi R22, 0
+	CP printByte, R22
+	BRSH print_0
+print_F:
+	LDI printByte, 0xE2
+	RJMP label_ret	
+print_E:
+	LDI printByte, 0xF2
+	RJMP label_ret	
+print_D:
+	LDI printByte, 0x7F
+	RJMP label_ret	
+print_C:
+	LDI printByte, 0x72
+	RJMP label_ret	
+print_B:
+	LDI printByte, 0xFF
+	RJMP label_ret	
+print_A:
+	LDI printByte, 0xEE
+	RJMP label_ret	
+print_9:
+	LDI printByte, 0xDE
+	RJMP label_ret	
+print_8:
+	LDI printByte, 0xFE
+	RJMP label_ret	
+print_7:
+	LDI printByte, 0x0E
+	RJMP label_ret
+print_6:
+	LDI printByte, 0xFA
+	RJMP label_ret	
+print_5:
+	LDI printByte, 0xDA
+	RJMP label_ret	
+print_4:
+	LDI printByte, 0xCC
+	RJMP label_ret	
+print_3:
+	LDI printByte, 0x9E
+	RJMP label_ret	
+print_2:
+	LDI printByte, 0xB6
+	RJMP label_ret	
+print_1:
+	LDI printByte, 0x0C
+	RJMP label_ret
+print_0:
+	LDI printByte, 0x7E
+	RJMP label_ret
+label_ret:
+	RET
 
-
-delay:							; 0.1mS * wreg
-		ldi duration, 24
-		cycle:
-		nop
-		dec 	duration
-		brne	cycle
-		dec 	wreg
-		brne	delay
-ret
+movBytes:
+	CLR lbyteNum
+	CLR hbyteNum
+	MOV hbyteNum, wreg
+	LSR hbyteNum
+	LSR hbyteNum
+	LSR hbyteNum
+	LSR hbyteNum
+	MOV lbyteNum, wreg
+	LSL lbyteNum
+	LSL lbyteNum
+	LSL lbyteNum
+	LSL lbyteNum
+	LSR lbyteNum
+	LSR lbyteNum
+	LSR lbyteNum
+	LSR lbyteNum
+	RET
    
+delay_setup:
+   CLR R21
+   CLR R22
+   CLR R23
+   CLR tmp_reg
+   LDI R21, 0 ; here is the hihest byte
+   LDI R22, 1
+   ;LDI R17, 250 ; here is the lowest byte
+   LSL R21
+   BST R22, 7
+   BLD R21, 0
+   LSL R22
+   LSL R21
+   BST R22, 7
+   BLD R21, 0
+   LSL R22
+delay_cycle:
+   SUBI R23, 1 ; 1 tick
+   SBCI R22, 0 ; 1 tick
+   SBCI R21, 0 ; 1 tick
+
+   CPSE R21, tmp_reg ; 1 ticks, if equal then skip (2 ticks)
+   RJMP wait_nop_8 ; 2 ticks
+   CPSE R22, tmp_reg ; 1 ticks, if equal then skip (2 ticks)
+   RJMP wait_nop_5 ; 2 ticks
+   CPSE R23, tmp_reg ; 1 ticks, if equal then skip (2 ticks)
+   RJMP wait_nop_2 ; 2 ticks
+   NOP
+   NOP
+   NOP
+   RET                        ; go back, 4 ticks
+wait_nop_8:
+   NOP
+   NOP
+   NOP
+wait_nop_5:
+   NOP
+   NOP
+   NOP
+wait_nop_2:
+   NOP
+   NOP
+   RJMP   delay_cycle
+   
+
 adc_convert:
 
 	STS ADMUX, wreg				; wreg contains channel
 	LDS R25, ADCSRA
-   OUT PORTB, R25
-	sbi PORTB, ADSC
-   IN R25, PORTB
+   OUT PORTD, R25
+	sbi PORTD, ADSC
+   IN R25, PORTD
    STS ADCSRA, R25
-
+cycle_adc:
    LDS R25, ADCSRA
-   OUT PORTB, R25
-	sbic PORTB , ADSC
-	rjmp PC-3
-   IN R25, PORTB
+   OUT PORTD, R25
+	sbic PORTD , ADSC
+	rjmp cycle_adc
+   IN R25, PORTD
    STS ADCSRA, R25
 
 
    LDS wreg, ADCL				; ������ ������������ (������ ������ �������� ADCL)
-	LDS wreg, ADCH				; wreg contains result
+	;OUT PORTD, wreg
+   ;ldi wreg, 250				; 25mS
+	;rcall delay
+   LDS wreg, ADCH				; wreg contains result
+   ;OUT PORTD, wreg
+   ;ldi wreg, 250		case 0:
+    ;out  PORTB,  0b11111110;
+     ; break; 	; 25mS
+	;rcall delay
 
 ret
 

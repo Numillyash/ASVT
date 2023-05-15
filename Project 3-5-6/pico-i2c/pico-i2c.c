9c216c4287cdf367e8a2593c2d9d7f80591e9569
germanFid <german.fidarov@gmail.com>
2023-05-15 11:17:44 +0300

    invent normal connecrtion?

--- Project 3-5-6/pico-i2c/pico-i2c.c
+++ Project 3-5-6/pico-i2c/pico-i2c.c
@@ -13,48 +13,73 @@ static const uint I2C_SLAVE_ADDRESS = 0x08;
 static const uint I2C_BAUDRATE = 100000; // 100 kHz
 static const uint I2C_SLAVE_SDA_PIN = PICO_DEFAULT_I2C_SDA_PIN; // 4
 static const uint I2C_SLAVE_SCL_PIN = PICO_DEFAULT_I2C_SCL_PIN; // 5
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+
 /* 
  * This include brings in static arrays which contain audio samples. 
  * if you want to know how to make these please see the python code
  * for converting audio samples into static arrays. 
  */
+ 
+#define bool uint8_t
+#define false 0
+#define true 1
 
+bool hadRecievedMessage = false;
+bool isPlaying = false;
 static struct
 {
+    uint8_t buffer[256];
+    uint8_t buffer_address;
     uint8_t mem[256];
     uint8_t mem_address;
-    bool mem_address_written;
+    bool IsRecieveStarted;
 } context;
 
 
-/*
- * PWM Interrupt Handler which outputs PWM level and advances the 
- * current sample. 
- * 
- * We repeat the same value for 8 cycles this means sample rate etc
- * adjust by factor of 8   (this is what bitshifting <<3 is doing)
- * 
- */
-// Our handler is called from the I2C ISR, so it must complete quickly. Blocking calls /
-// printing to stdio may interfere with interrupt handling.
 static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
-    switch (event) {
-    case I2C_SLAVE_RECEIVE: // master has written some data
-        // save into memory
-        context.mem[context.mem_address] = i2c_read_byte_raw(i2c);
-        context.mem_address++;
-        break;
-    case I2C_SLAVE_REQUEST: // master is requesting data
-        // load from memory
-        i2c_write_byte_raw(i2c, context.mem[context.mem_address]);
-        context.mem_address++;
-        break;
-    case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
-        context.mem_address = 0;
-        printf("SLAVE DONE\n");
-        break;
-    default:
-        break;
+    if(!hadRecievedMessage){
+        switch (event) {
+            case I2C_SLAVE_RECEIVE: // master has written some data
+                // save into memory
+                context.buffer[context.buffer_address] = i2c_read_byte_raw(i2c);
+                context.buffer_address ++;
+                // context.mem[context.mem_address] = i2c_read_byte_raw(i2c);
+                // context.mem_address++;
+                break;
+            case I2C_SLAVE_REQUEST: // master is requesting data
+                // load from memory
+                i2c_write_byte_raw(i2c, context.mem[context.mem_address]);
+                context.mem_address++;
+                break;
+            case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
+                context.buffer_address = 0;
+                // обработка входящего сообщения
+                int isMessageStart = context.buffer[0] == 0 ? 1 : 0;
+                int isMessageEnd = context.buffer[0] == 2 ? 1 : 0;
+                if(isMessageStart)
+                {
+                    memset(context.mem, 0, 256);
+                    context.mem_address = 0;
+                }
+                else if(isMessageEnd)
+                {
+                    hadRecievedMessage = true;
+                }
+                else
+                {
+                    strncpy(context.mem, context.buffer+2, context.buffer[1]);
+                    printf("%s\n", context.mem);
+                    context.mem_address = context.buffer_address - 2;
+                }
+                memset(context.buffer, 0, 256);
+                context.buffer_address = 0;
+                printf("SLAVE DONE\n");
+                
+                break;
+            default:
+                break;
+        }
     }
 }
 
@@ -71,25 +96,71 @@ static void setup_slave() {
     // configure I2C0 for slave mode
     i2c_slave_init(i2c0, I2C_SLAVE_ADDRESS, &i2c_slave_handler);
 }
+void pwm_interrupt_handler() {
+    pwm_clear_irq(pwm_gpio_to_slice_num(LED_PIN));    
+        pwm_set_gpio_level(LED_PIN, context.mem[context.mem_address]*10);  
+        context.mem_address++;
+}
+void playSample(int n)
+{
+    printf("Playing %d sample\n", n);
+
+    sleep_ms(1000);
+}
 
 int main() {
     stdio_init_all();
     setup_slave();
 
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_set_function(LED_PIN, GPIO_FUNC_PWM);
 
+    int audio_pin_slice = pwm_gpio_to_slice_num(LED_PIN);
 
+    // Setup PWM interrupt to fire when PWM cycle is complete
+    pwm_clear_irq(audio_pin_slice);
+    pwm_set_irq_enabled(audio_pin_slice, true);
+    // set the handle function above
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler); 
+    irq_set_enabled(PWM_IRQ_WRAP, true);
+
+    // Setup PWM for audio output
+    pwm_config config = pwm_get_default_config();
+    /* Base clock 176,000,000 Hz divide by wrap 250 then the clock divider further divides
+     * to set the interrupt rate. 
+     * 
+     * 11 KHz is fine for speech. Phone lines generally sample at 8 KHz
+     * 
+     * 
+     * So clkdiv should be as follows for given sample rate
+     *  8.0f for 11 KHz
+     *  4.0f for 22 KHz
+     *  2.0f for 44 KHz etc
+     */
+    pwm_config_set_clkdiv(&config, 8.0f); 
+    pwm_config_set_wrap(&config, 250); 
+    pwm_init(audio_pin_slice, &config, true);
+
+    pwm_set_gpio_level(LED_PIN, 0);
 
     while(true)
     {
+        if(hadRecievedMessage)
+        {
+            for(int i = 0; i < msg1_len; i++)
+        {
+            playSample((int)message1[i]);
+        }
+        hadRecievedMessage = false;
+        }
+        else{
         gpio_put(LED_PIN, 1);
         sleep_ms(250);
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
         //__wfi(); // Wait for Interrupt
-    }
+    }}
 }
 
 

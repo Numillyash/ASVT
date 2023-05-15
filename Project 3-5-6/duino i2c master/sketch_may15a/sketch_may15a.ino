0a398f81de2fb830d46f4b3d5e76f221f9b1f229
germanFid <german.fidarov@gmail.com>
2023-05-15 17:09:00 +0300

    Add message rx_tx

--- Project 3-5-6/duino i2c master/sketch_may15a/sketch_may15a.ino
+++ Project 3-5-6/duino i2c master/sketch_may15a/sketch_may15a.ino
@@ -1,6 +1,12 @@
 #include <Wire.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(7, 8);
+
 #define MESSAGE "ABCDEFGHIJKLMNOPQRSTUVWXYZ"//"JOINDARKIKIZI"
 #define MLEN 26
+
+#define _SS_MAX_RX_BUFF 256
 
 void messageFunction(uint8_t msgLen, const char* msg)
 {
@@ -14,7 +20,6 @@ void messageFunction(uint8_t msgLen, const char* msg)
     char requestTaken[256] = "";
 transmission:
     Serial.println("Begin main");
-    delay(2000);
     Wire.beginTransmission(8); // transmit to device #8
     Wire.write(':');             // Send "MSG"
     Wire.write(msgLen + 1);    // Send Length up to 255
@@ -49,10 +54,68 @@ void setup()
     // put your setup code here, to run once:
     Wire.begin();       // join I2C bus (address optional for master)
     Serial.begin(9600); // start serial for output
+    mySerial.begin(9600);
+
+    Serial.println("Initializing GPRS...");
+    delay(1000);
+
+    mySerial.println("AT"); updateSerial();
+    mySerial.println("AT+CSQ"); updateSerial();
+
+    // Прочитать информацию о SIM карте, чтобы убедиться, что SIM карта подключена
+    mySerial.println("AT+CCID"); updateSerial();
 }
 
 void loop()
 {
-  messageFunction(MLEN, MESSAGE);
+  updateSerial();
+  // messageFunction(MLEN, MESSAGE);
   delay(5000);
+}
+
+bool prefix(const char *pre, const char *str)
+{
+    return strncmp(pre, str, strlen(pre)) == 0;
+}
+
+void updateSerial()
+{
+  char buf[256];
+  bool flagMsg = false;
+
+  delay(500);
+
+  while (Serial.available()) 
+  {
+    // Пересылка того, что было получено с аппаратного последовательного порта, 
+    // на программный последовательный порт
+    mySerial.write(Serial.read());
+  }
+  while(mySerial.available()) 
+  {
+    // Пересылка того, что было получено с программного последовательного порта, 
+    // на аппаратный последовательный порт
+    Serial.write(mySerial.read(buf));
+
+    if (prefix("+CMT", buf))
+    {
+      flagMsg = true;
+      continue;
+    }
+
+    if (flagMsg)
+    {
+      // To uppercase
+      char *s = buf;
+      int ln = 0;
+      while (*s)
+      {
+        *s = toupper((unsigned char) *s);
+        s++;
+        ln++;
+      }
+      
+      messageFunction(ln, buf);
+    }
+  }
 }

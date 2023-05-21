8e4617c969c963fc51528a211c1af58ff73c9dbc
Numillyash <ulanovsky.zhora@ya.ru>
2023-05-21 18:57:58 +0300

    Готово к отчету

--- Project 3-5-6/duino_I2C_test/duino_I2C_test.ino
+++ Project 3-5-6/duino_I2C_test/duino_I2C_test.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
 
+#define _SS_MAX_RX_BUFF 256
+
 SoftwareSerial mySerial(7, 8);
 
 #define MESSAGE "ABCDEFGHIJKLMNOPQRSTUVWXYZ" //"JOINDARKIKIZI"
@@ -17,6 +19,7 @@ void messageFunction(uint8_t msgLen, const char *msg)
   // String requestReal = ":" + (char)msgLen + msg + "\n";
   char requestTaken[256] = "";
 transmission:
+  delay(2000);
   Serial.println("Begin main");
   Wire.beginTransmission(8); // transmit to device #8
   Wire.write(':');           // Send "MSG"
@@ -27,12 +30,13 @@ transmission:
 
   Wire.requestFrom((uint8_t)8, msgLen); // request 6 bytes from slave device #8
   int rt_len = 0;
-  
+
   while (Wire.available())
-  {                       // slave may send less than requested
+  { // slave may send less than requested
     char c = Wire.read(); // receive a byte as character
     requestTaken[rt_len++] = c;
     Serial.print((int)c);
+    Serial.print(" ");
   }
 
   Serial.println("");
@@ -59,7 +63,7 @@ void setup()
   // put your setup code here, to run once:
   Wire.begin();       // join I2C bus (address optional for master)
   Serial.begin(9600); // start serial for output
-  mySerial.begin(9600);
+  mySerial.begin(19200);
 
   Serial.println("Initializing GPRS...");
   delay(1000);
@@ -69,6 +73,12 @@ void setup()
 
   // Прочитать информацию о SIM карте, чтобы убедиться, что SIM карта подключена
   mySerial.println("AT+CCID"); updateSerial();
+  // Настройка текствого режима
+  mySerial.println("AT+CMGF=1");
+  updateSerial();
+  // Решает, как следует обрабатывать новые входящие SMS сообщения
+  mySerial.println("AT+CNMI=1,2,0,0,0");
+  updateSerial();
 }
 
 void loop()
@@ -82,48 +92,75 @@ void loop()
 void updateSerial()
 {
   char buf[256];
+  memset(buf, 0, 256);
   bool flagMsg = false;
 
   delay(500);
 
-  while (Serial.available()) 
+  while (Serial.available())
   {
-    // Пересылка того, что было получено с аппаратного последовательного порта, 
+    // Пересылка того, что было получено с аппаратного последовательного порта,
     // на программный последовательный порт
     mySerial.write(Serial.read());
   }
 
-  while(mySerial.available()) 
+  int ln = 0;
+  while (mySerial.available())
   {
-    // Пересылка того, что было получено с программного последовательного порта, 
+    Serial.print("\n Avaible = ");
+    Serial.print(mySerial.available()); 
+    Serial.print("\nWhile start here: ln = ");
+    Serial.println(ln);
+    // Пересылка того, что было получено с программного последовательного порта,
     // на аппаратный последовательный порт
-    Serial.write(mySerial.read(buf));
+    buf[ln++] = mySerial.read();
+    Serial.write(buf[ln - 1]);
 
-    if (prefix("+CMT", buf))
+    Serial.print((int)buf[ln - 1]);
+    
+    if (buf[ln - 1] == '\x10' || buf[ln - 1] == '\n')
     {
-      flagMsg = true;
-      continue;
-    }
-
-    if (flagMsg)
-    {
-      // To uppercase
-      char *s = buf;
-      int ln = 0;
-      while (*s)
+      Serial.write("\nEND OF STR\n");
+      ln = 0;
+      if (prefix("+CMT", buf))
       {
-        *s = toupper((unsigned char) *s);
-        s++;
-        ln++;
-      }
-      
-      messageFunction(ln, buf);
-      break;
+        flagMsg = true;
+        Serial.write("\nWE GOT MSG\n");
+        memset(buf, 0, 256);
+        continue;
+        
+      } 
     }
+    Serial.print("\nWhile end here\n");
   }
+  ln+=1;
+  Serial.println("NO CYCLE HERE");
+  Serial.println("Some buffer");
+  for(int i = 0; i < ln; i++)
+  {
+      Serial.write(buf[i]);
+  }
+  Serial.println(buf);
+  if (flagMsg)
+      {
+        Serial.write("\nWE sent MSG\n");
+        // To uppercase
+        char *s = buf;
+        ln = 0;
+        while (*s)
+        {
+          *s = toupper((unsigned char) * s);
+          s++;
+          ln++;
+        }
+        buf[ln-2] = '\0';
+        ln -= 2;
+        messageFunction(ln, buf);
+        flagMsg = false;
+      }
 }
 
 bool prefix(const char *pre, const char *str)
 {
-    return strncmp(pre, str, strlen(pre)) == 0;
+  return strncmp(pre, str, strlen(pre)) == 0;
 }
